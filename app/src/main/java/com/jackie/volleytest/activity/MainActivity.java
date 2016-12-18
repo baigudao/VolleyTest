214cfc9971aa6e3f229e840b6c0455a4e4aebd2a
jackie <971060378@qq.com>
2016-12-19 02:00:53 +0800

    实现了HTTP的GET、POST请求，图片的加载和缓存。没有运用自定义的XMLRequest。

--- app/src/main/java/com/jackie/volleytest/activity/MainActivity.java
+++ app/src/main/java/com/jackie/volleytest/activity/MainActivity.java
@@ -1,18 +1,32 @@
 package com.jackie.volleytest.activity;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.jackie.volleytest.R;
-import com.jackie.volleytest.util.MyStringRequest;
+import com.jackie.volleytest.util.BitmapCache;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016/12/18.
@@ -20,8 +34,13 @@ import com.jackie.volleytest.util.MyStringRequest;
 public class MainActivity extends Activity {
 
     private Button button;
+    private Button button_json;
+    private Button button_image;
+    private Button button_image_load;
+    private ImageView imageView;
     private TextView textView;
     private RequestQueue requestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +48,11 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.btn);
+        button_json = (Button) findViewById(R.id.btn_json);
         textView = (TextView) findViewById(R.id.tv_content);
+        button_image = (Button) findViewById(R.id.btn_image);
+        button_image_load = (Button) findViewById(R.id.btn_image_load);
+        imageView = (ImageView) findViewById(R.id.iv_image);
 
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -38,14 +61,32 @@ public class MainActivity extends Activity {
                 loadNetContent();
             }
         });
-
+        button_json.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadJson();
+            }
+        });
+        button_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadImage();
+            }
+        });
+        button_image_load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadImageLoader();
+            }
+        });
         requestQueue = Volley.newRequestQueue(this);
     }
 
 
     private void loadNetContent() {
 
-        MyStringRequest myStringRequest = new MyStringRequest("http://www.ifeng.com", new Response.Listener<String>() {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET
+                , "http://ifeng.com", new Response.Listener<String>() {
             @Override
             public void onResponse(String s) {
                 textView.setText(s);
@@ -53,9 +94,93 @@ public class MainActivity extends Activity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError volleyError) {
+
+            }
+        }) {
+            protected final String TYPE_UTF8_CHARSET = "charset=UTF-8";
+            // 重写parseNetworkResponse方法改变返回头参数解决乱码问题
+            // 主要是看服务器编码，如果服务器编码不是UTF-8的话那么就需要自己转换，反之则不需要
+            @Override
+            protected Response<String> parseNetworkResponse(
+                    NetworkResponse response) {
+                try {
+                    String type = response.headers.get("Content-Type");
+                    if (type == null) {
+                        type = TYPE_UTF8_CHARSET;
+                        response.headers.put("Content-Type", type);
+                    } else if (!type.contains("UTF-8")) {
+                        type += ";" + TYPE_UTF8_CHARSET;
+                        response.headers.put("Content-Type", type);
+                    }
+                } catch (Exception e) {
+                }
+                return super.parseNetworkResponse(response);
+            }
+        };
+        stringRequest.setShouldCache(true);//启用缓存
+        requestQueue.add(stringRequest);
+    }
+
+    private void postRequest (){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "xxx", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+
+            }
+        }){
+            //设置post请求的参数
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> map = new HashMap<>();
+                map.put("m","admin");
+                map.put("p","123456");
+                return map;
+            }
+        };
+        stringRequest.setShouldCache(true);
+        requestQueue.add(stringRequest);
+    }
+
+    private void loadJson() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("http://www.weather.com.cn/data/cityinfo/101010100.html",
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+
+                textView.setText(jsonObject.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
                 Toast.makeText(MainActivity.this, volleyError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
-        requestQueue.add(myStringRequest);
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    private void loadImage() {
+        ImageRequest imageRequest = new ImageRequest("http://e.hiphotos.baidu.com/image/pic/item/d1160924ab18972b40187fcbe4cd7b899e510a94.jpg", new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap bitmap) {
+                imageView.setImageBitmap(bitmap);
+            }
+        }, 0, 0, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(MainActivity.this, volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestQueue.add(imageRequest);
+    }
+
+    private void loadImageLoader() {
+        ImageLoader imageLoader = new ImageLoader(requestQueue, new BitmapCache());
+        ImageLoader.ImageListener imageListener = ImageLoader.getImageListener(imageView, R.drawable.no_hot_food, R.drawable.ic_share_close);
+        imageLoader.get("http://b.hiphotos.baidu.com/image/pic/item/7e3e6709c93d70cf5a0cc307fadcd100bba12bc1.jpg", imageListener);
     }
 }
